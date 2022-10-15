629eaeb53afc3f3966ee6f919cb7b83da3efad04
Ludvig Dodi <ludvigdodi.job@gmail.com>
2022-10-15 18:37:27 +0300

    first class added

--- bloodhound_bot.py
+++ bloodhound_bot.py
@@ -1,9 +1,7 @@
 import requests
 from bot_token import token
 
-root_url = "https://api.telegram.org/bot"
 ok_codes = 200, 201, 202, 203, 204
-
 
 user = {"username": "Ludvig", "level": 1}
 
@@ -57,46 +55,53 @@ def create_result_message(matched_sentences: list) -> str:
     return result_message
 
 
-def send_message(token, chat_id, message):
-    url = f"{root_url}{token}/sendMessage"
-    res = requests.post(url, data={"chat_id": chat_id, "text": message})
-    if res.status_code in ok_codes:
-        return True
-    else:
-        print(f"Request failed with status_code {res.status_code}")
-        return False
+class Bot:
+    root_url = "https://api.telegram.org/bot"
+
+    def __init__(self, token=None):
+        self.token = token
+
+    def get_updates(self):
+        url = f"{self.root_url}{self.token}/getUpdates"
+        res = requests.get(url)
+
+        if res.status_code in ok_codes:
+            updates = res.json()
+            return updates
+
+    def send_message(self, chat_id, message):
+        url = f"{self.root_url}{self.token}/sendMessage"
+        res = requests.post(url, data={"chat_id": chat_id, "text": message})
+        if res.status_code in ok_codes:
+            return True
+        else:
+            print(f"Request failed with status_code {res.status_code}")
+            return False
+
+    def process_message(self, message: str) -> str:
+        """обрабтывает входящее сообщение и выдает ответ, который будет отправлен юзеру"""
+        matched_sentences = fill_matched_sentences(
+            message=message, user=user, sentences=sentences
+        )
+        message = create_result_message(matched_sentences)
+        return message
+
+    def poolling(self):
+        last_message_id = 0
+
+        while True:
+            updates = self.get_updates()
+            last_message = updates["result"][-1]
+            message_id = last_message["message"]["message_id"]
+
+            last_message_text = last_message["message"]["text"]
+            chat_id = last_message["message"]["chat"]["id"]
+
+            if message_id > last_message_id:
+                message_to_user = self.process_message(last_message_text)
+                self.send_message(chat_id, message_to_user)
+                last_message_id = message_id
 
 
-def get_updates(token):
-    url = f"{root_url}{token}/getUpdates"
-    res = requests.get(url)
-    if res.status_code in ok_codes:
-        updates = res.json()
-        return updates
-
-
-def process_message(message: str) -> str:
-    """обрабтывает входящее сообщение и выдает ответ, который будет отправлен юзеру"""
-    matched_sentences = fill_matched_sentences(
-        message=message, user=user, sentences=sentences
-    )
-    message = create_result_message(matched_sentences)
-    return message
-
-
-updates = get_updates(token)
-
-
-last_message_id = 0
-while True:
-    updates = get_updates(token)
-    last_message = updates["result"][-1]
-    message_id = last_message["message"]["message_id"]
-
-    last_message_text = last_message["message"]["text"]
-    chat_id = last_message["message"]["chat"]["id"]
-
-    if message_id > last_message_id:
-        message_to_user = process_message(last_message_text)
-        send_message(token, chat_id, message_to_user)
-        last_message_id = message_id
+bot = Bot(token)
+bot.poolling()
