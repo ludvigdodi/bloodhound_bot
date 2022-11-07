e786ec94f76c02157df74a80af68aaf28f0c3527
Ludvig Dodi <ludvigdodi.job@gmail.com>
2022-11-07 23:13:46 +0200

    added token

---
+++ bot_token.py
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+# token = "5645942050:AAFeK453jVLJfdAlg2359HR4NYjB2vU9A9Q"
+
+token = "5645942050:AAFeK453jVLJfdAlg2359HR4NYjB2vU9A9Q"
+=======
+from os import environ as env
+
+# token = "5645942050:AAFeK453jVLJfdAlg2359HR4NYjB2vU9A9Q"
+
+token = env.get("TOKEN")
+assert token != None
+>>>>>>> a606f1ff0e3b87e8826033472c2133ba909cb9d6
