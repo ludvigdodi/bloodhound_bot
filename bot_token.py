852cd7929aaed6cec4d5158228cd1a1833e7b297
ludvigdodi <65244894+ludvigdodi@users.noreply.github.com>
2022-11-07 22:28:23 +0200

    Revert "Added dockerfile"

--- bot_token.py
+++ bot_token.py
@@ -1,6 +1,1 @@
-from os import environ as env
-
-# token = "5488524499:AAG1Jsp4ESZ_MRpPVLbqeOM12WEhrj-MXIg"
-
-token = env.get("TOKEN")
-assert token != None
+token = "5488524499:AAG1Jsp4ESZ_MRpPVLbqeOM12WEhrj-MXIg"
