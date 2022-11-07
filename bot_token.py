4ca8d6b34d7390ed4976613be13d2eccd9db3b62
Yahor Chuchvaga <johndoe@example.com>
2022-11-07 20:37:36 +0100

    added dockerfile

--- bot_token.py
+++ bot_token.py
@@ -1,1 +1,6 @@
-token = "5488524499:AAG1Jsp4ESZ_MRpPVLbqeOM12WEhrj-MXIg"
+from os import environ as env
+
+# token = "5488524499:AAG1Jsp4ESZ_MRpPVLbqeOM12WEhrj-MXIg"
+
+token = env.get("TOKEN")
+assert token != None
