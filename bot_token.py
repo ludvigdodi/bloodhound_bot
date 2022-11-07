337652d1db59193149505512a62c05dc0839b0f0
Ludvig Dodi <ludvigdodi.job@gmail.com>
2022-11-07 23:15:57 +0200

    token changed

--- bot_token.py
+++ bot_token.py
@@ -5,8 +5,6 @@ token = "5645942050:AAFeK453jVLJfdAlg2359HR4NYjB2vU9A9Q"
 =======
 from os import environ as env
 
-# token = "5645942050:AAFeK453jVLJfdAlg2359HR4NYjB2vU9A9Q"
-
 token = env.get("TOKEN")
 assert token != None
 >>>>>>> a606f1ff0e3b87e8826033472c2133ba909cb9d6
